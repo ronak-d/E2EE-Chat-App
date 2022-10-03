94d21ac4bc03141bad5bd297914d87bf752f678e
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-10-03 18:54:54 +0530

    encrytion p

---
+++ frontend/src/App.js
@@ -0,0 +1,67 @@
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+// import { nanoid } from "nanoid";
+
+// no dotenv
+const socket = io.connect("http://localhost:5000");
+
+const EncryptRsa = require('encrypt-rsa').default;
+const encryptRsa = new EncryptRsa();
+
+const { privateKey, publicKey } = encryptRsa.createPrivateAndPublicKeys();
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+  const [username, setUsername] = useState("");
+
+  // Main thing just handling with the socket. 
+  const sendChat = (e) => {    
+    e.preventDefault();
+    socket.emit("chat", { message, username });
+    setMessage("");
+  };
+
+  useEffect(() => {
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload]);
+    });
+  });
+
+  useEffect(() => {
+    const promptValue = prompt(`Enter Username ${username}`);
+    setUsername(promptValue);
+  },[]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>E2E Chat app</h1>
+        {chat.map((payload, index) => {
+          return (
+            <p key={index}>
+              {payload.message} <span> User: {payload.username}</span>
+            </p>
+          );
+        })}
+
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+            name="chat"
+            placeholder="send text"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
